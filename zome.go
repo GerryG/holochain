41cbb61a2a80c61054fc37bea5ccb349471a30c1
Gerry Gleason <ggleason@tribpub.com>
2017-04-23 00:53:26 +0000

    Move *Def to zome

--- zome.go
+++ zome.go
@@ -49,6 +49,22 @@ type FunctionDef struct {
 	CallingType string
 }
 
+// EntryDef struct holds an entry definition
+type EntryDef struct {
+	Name       string
+	DataFormat string
+	Schema     string // file name of schema or language schema directive
+	SchemaHash Hash
+	Sharing    string
+	validator  SchemaValidator
+}
+
+// FunctionDef holds the name and calling type of an DNA exposed function
+type FunctionDef struct {
+	Name        string
+	CallingType string
+}
+
 // ZomePath returns the path to the zome dna data
 // @todo sanitize the name value
 func (h *Holochain) ZomePath(z *Zome) string {
